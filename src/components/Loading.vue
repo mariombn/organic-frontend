c6587cfe40c56abe1a52bb7882a8bd003bbcd3ca
Mario de Moraes <mariombn@gmail.com>
2023-07-18 20:58:43 -0300

    Usando overflow do Vuetify

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,7 +1,14 @@
 <template>
-    <div v-if="active" class="overlay">
-        <div class="loader"></div>
-    </div>
+    <v-overlay
+      :model-value="active"
+      class="align-center justify-center"
+    >
+      <v-progress-circular
+        color="primary"
+        indeterminate
+        size="64"
+      ></v-progress-circular>
+    </v-overlay>
   </template>
   
   <script>
@@ -15,38 +22,3 @@
   };
   </script>
   
-  <style scoped>
-.overlay {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 9999;
-}
-
-.loader {
-  border: 16px solid #f3f3f3;
-  border-radius: 50%;
-  border-top: 16px solid #3498db;
-  width: 120px;
-  height: 120px;
-  -webkit-animation: spin 2s linear infinite; /* Safari */
-  animation: spin 2s linear infinite;
-}
-
-/* Safari */
-@-webkit-keyframes spin {
-  0% { -webkit-transform: rotate(0deg); }
-  100% { -webkit-transform: rotate(360deg); }
-}
-
-@keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
-}
-</style>
